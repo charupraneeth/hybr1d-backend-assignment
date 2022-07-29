82073d55cbf100da1cb8dc9e7cffd5be44254897
charupraneeth <charupraneeth@gmail.com>
2022-07-29 13:04:54 +0530

    add hashing to user registration

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,18 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
+const SALT_ROUNDS = 10;
 const router = Router();
 const prisma = new PrismaClient();
 
 const userSchema = z.object({
   username: z.string().min(3),
-  password: z.string().min(10),
+  password: z.string().min(8),
 });
 router.post("/register", async (req, res, next) => {
   try {
     // console.log(userSchema.parse(req.body));
     const parsedUser = userSchema.parse(req.body);
+
+    const hashedPassword = await bcrypt.hash(parsedUser.password, SALT_ROUNDS);
+    parsedUser.password = hashedPassword;
 
     const user = await prisma.user.create({
       data: parsedUser,
