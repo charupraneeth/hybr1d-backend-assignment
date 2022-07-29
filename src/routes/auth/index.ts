787bed3e58e711e5834a31b9834dcd7a4cac0b9e
charupraneeth <charupraneeth@gmail.com>
2022-07-29 13:12:27 +0530

    add login route w/o jwt

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -38,9 +38,33 @@ router.post("/register", async (req, res, next) => {
   }
 });
 
-router.post("/login", (req, res) => {
-  res.json({
-    message: "In login route",
-  });
+router.post("/login", async (req, res, next) => {
+  try {
+    const parsedUser = userSchema.parse(req.body);
+
+    const user = await prisma.user.findUniqueOrThrow({
+      where: {
+        username: parsedUser.username,
+      },
+    });
+
+    const isMatched = await bcrypt.compare(parsedUser.password, user.password);
+
+    if (!isMatched) {
+      throw new Error("Incorrect username/password");
+    }
+
+    user.password = "";
+
+    res.json({
+      status: "ok",
+      data: {
+        user,
+      },
+      error: "",
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 export default router;
