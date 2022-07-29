bdb6016424377682f924f6ece3ce625a27508107
charupraneeth <charupraneeth@gmail.com>
2022-07-29 20:31:09 +0530

    add get catalog by sellerId route

--- src/routes/buyer/index.ts
+++ src/routes/buyer/index.ts
@@ -27,4 +27,31 @@ router.get("/list-of-sellers", async (req, res, next) => {
   }
 });
 
+router.get("/seller-catalog/:seller_id", async (req, res, next) => {
+  try {
+    const sellerId = req.params.seller_id;
+
+    const [seller] = await prisma.user.findMany({
+      where: {
+        id: parseInt(sellerId),
+        type: "seller",
+      },
+      select: {
+        id: true,
+        username: true,
+        catalog: true,
+      },
+    });
+
+    res.json({
+      status: "ok",
+      data: {
+        seller,
+      },
+      error: "",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 export default router;
