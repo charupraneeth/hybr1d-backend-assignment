613e70aa2dae040966a38c26b3ef06782ce29d68
charupraneeth <charupraneeth@gmail.com>
2022-07-29 22:56:18 +0530

    add create-order route

--- src/routes/buyer/index.ts
+++ src/routes/buyer/index.ts
@@ -1,8 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
+import { z } from "zod";
 
 const router = Router();
 const prisma = new PrismaClient();
+
+const productIdsSchema = z.object({
+  productIds: z.array(z.number()),
+});
 
 router.get("/list-of-sellers", async (req, res, next) => {
   try {
@@ -29,7 +34,7 @@ router.get("/list-of-sellers", async (req, res, next) => {
 
 router.get("/seller-catalog/:seller_id", async (req, res, next) => {
   try {
-    const sellerId = req.params.seller_id;
+    const sellerId = req.params?.seller_id;
 
     const [seller] = await prisma.user.findMany({
       where: {
@@ -54,4 +59,54 @@ router.get("/seller-catalog/:seller_id", async (req, res, next) => {
     next(error);
   }
 });
+
+router.post("/create-order/:seller_id", async (req, res, next) => {
+  try {
+    const sellerId = req.params?.seller_id;
+    const parsedProductIds = productIdsSchema.parse(req.body);
+
+    const [seller] = await prisma.user.findMany({
+      where: {
+        id: parseInt(sellerId),
+        type: "seller",
+      },
+      select: {
+        id: true,
+        catalog: true,
+      },
+    });
+
+    const catalogProducts = new Set(
+      seller.catalog.map((product) => product.id)
+    );
+
+    for (let id of parsedProductIds.productIds) {
+      if (!catalogProducts.has(id)) {
+        throw new Error(`Product with id : ${id} not found`);
+      }
+    }
+
+    const order = await prisma.order.create({
+      data: {
+        buyerId: seller.id,
+        products: {
+          connect: parsedProductIds.productIds.map((id) => {
+            return { id };
+          }),
+        },
+      },
+    });
+
+    res.json({
+      status: "ok",
+      data: {
+        order,
+      },
+      error: "",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 export default router;
